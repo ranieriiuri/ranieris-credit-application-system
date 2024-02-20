b8268ee46f5b98e9db04d9f807ea9d2d0eb60f8f
ranieriiuri <iuriranierioliveira@gmail.com>
2024-02-20 16:27:06 -0300

    CustomerResource update method tested

--- src/test/kotlin/api/ranieriiuri/credit/application/system/controller/CustomerResourceTest.kt
+++ src/test/kotlin/api/ranieriiuri/credit/application/system/controller/CustomerResourceTest.kt
@@ -1,6 +1,7 @@
 package api.ranieriiuri.credit.application.system.controller
 
 import api.ranieriiuri.credit.application.system.dto.CustomerDto
+import api.ranieriiuri.credit.application.system.dto.CustomerUpdateDto
 import api.ranieriiuri.credit.application.system.entity.Customer
 import api.ranieriiuri.credit.application.system.repository.CustomerRepository
 import com.fasterxml.jackson.databind.ObjectMapper
@@ -154,6 +155,48 @@ class CustomerResourceTest {
 
     }
 
+    @Test           // testando update customer (caminho feliz)
+    fun `should find customer by id, update this customer and returns 200 status`() {
+        //given
+        val customer: Customer = customerRepository.save(builderCustomerDto().toEntity())       // cria o customer e insere no DB
+        val customerUpdateDto: CustomerUpdateDto = builderCustomerUpdateDto()                   // cria o customer Dto
+        val valueAsString: String =
+            objectMapper.writeValueAsString(customerUpdateDto)                                  // transforma o customer Dto em json p enviar na req
+        //then & when
+        mockMvc.perform(MockMvcRequestBuilders.patch("$URL?customerId=${customer.id}")      // como usamos o '@RequestParam' a sintaxe é com a '?'
+            .contentType(MediaType.APPLICATION_JSON)
+            .content(valueAsString))                                                                   // passa esse customer Dto transformado em json
+            .andExpect(MockMvcResultMatchers.status().isOk)
+            .andExpect(MockMvcResultMatchers.jsonPath("$.firstName").value("Apollo"))   // comparando campos
+            .andExpect(MockMvcResultMatchers.jsonPath("$.income").value(2000))   // comparando campos
+            .andExpect(MockMvcResultMatchers.jsonPath("$.street").value("4th Avenue Boulevard"))   // comparando campos
+            .andDo(MockMvcResultHandlers.print())
+    }
+
+    @Test           // testando update (cenário de id inválido)
+    fun `should not update a customer with invalid id and return 400 status`() {
+        //given
+        val customerUpdateDto: CustomerUpdateDto = builderCustomerUpdateDto()                   // cria o customer Dto
+        val valueAsString: String =
+            objectMapper.writeValueAsString(customerUpdateDto)                                  // transforma o customer Dto em json p enviar na req
+        //then & when
+        mockMvc.perform(
+            MockMvcRequestBuilders.patch("$URL?customerId=${1}")      // como usamos o '@RequestParam' a sintaxe é com a '?'
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(valueAsString)
+        )                                                                   // passa esse customer Dto transformado em json
+            .andExpect(MockMvcResultMatchers.status().isBadRequest)
+            .andExpect(MockMvcResultMatchers.jsonPath("$.title").value("Bad Request! Consult the documentation"))  // se retorna essa msg
+            .andExpect(MockMvcResultMatchers.jsonPath("$.timestamp").exists())      // se tem 'timestamp'
+            .andExpect(MockMvcResultMatchers.jsonPath("$.status").value(400))   // se o status é o 400
+            .andExpect(
+                MockMvcResultMatchers.jsonPath("$.exception")
+                    .value("BusinessException"))            // se retona a Exception q criamos
+            .andDo(MockMvcResultHandlers.print())
+    }
+
+    // OBS.: Ainda podemos fazer testes do update, como 'passar campos em branco', entre outros...
+
 
     // fun que cria um Dto de teste
     fun builderCustomerDto(
@@ -176,5 +219,19 @@ class CustomerResourceTest {
         street = street
     )
 
+    // fun que será usada nos testes de update (novo Dto)
+    fun builderCustomerUpdateDto(
+        firstName: String = "Apollo",
+        lastName: String = "twelve",
+        income: BigDecimal = BigDecimal.valueOf(2000),
+        zipCode: String = "12345-000",
+        street: String = "4th Avenue Boulevard"
+    ): CustomerUpdateDto = CustomerUpdateDto(
+        firstName = firstName,
+        lastName = lastName,
+        income = income,
+        zipCode = zipCode,
+        street = street
+    )
 
 }
